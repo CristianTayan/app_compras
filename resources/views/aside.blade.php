62f042df16bcee6479bcca01496ead75245be648
CristianTayan <cris17j@gmail.com>
2020-05-15 23:55:39 -0500

    links

--- resources/views/aside.blade.php
+++ resources/views/aside.blade.php
@@ -21,30 +21,31 @@
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">Administrador</li>
         <!-- Optionally, you can add icons to the links -->
-      <li ><a href="{{Route('categorias.index')}}"><i class="fa fa-list"></i> <span>Categorias</span></a></li>
-      <li ><a href="{{Route('Empresas.index')}}"><i class="fa fa-building"></i> <span>Empresas</span></a></li>
+      <li class="{{ !Route::is('categorias.index') ?: 'active'}}"><a href="{{Route('categorias.index')}}"><i class="fa fa-list"></i> <span>Categorias</span></a></li>
+      <li class="{{ !Route::is('Empresas.index') ?: 'active'}}"><a href="{{Route('Empresas.index')}}"><i class="fa fa-building"></i> <span>Empresas</span></a></li>
+      <li ><a href=""><i class="fa fa-building"></i> <span>Pedido</span></a></li>
 
-      <li class="treeview">
+      <li class="treeview {{ !Route::is('Usuarios.index') ?: 'active' }} || {{!Route::is('Usuarios.indexA') ?: 'active'}} || {{!Route::is('Usuarios.indexP') ?: 'active'}}">
         <a href="{{Route('Usuarios.index')}}"><i class="fa fa-user"></i> <span>Usuarios</span>
           <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
         </a>
         <ul class="treeview-menu">
-          <li><a href="{{Route('Usuarios.index')}}">Usuarios aplicación</a></li>
-          <li><a href="{{Route('Usuarios.indexA')}}">Administradores</a></li>
-          <li><a href="{{Route('Usuarios.indexP')}}">Proveedores</a></li>
+          <li class="{{ !Route::is('Usuarios.index') ?: 'active'}}"><a href="{{Route('Usuarios.index')}}">Usuarios aplicación</a></li>
+          <li class="{{ !Route::is('Usuarios.indexA') ?: 'active'}}"><a href="{{Route('Usuarios.indexA')}}">Administradores</a></li>
+          <li class="{{ !Route::is('Usuarios.indexP') ?: 'active'}}"><a href="{{Route('Usuarios.indexP')}}">Proveedores</a></li>
         </ul>
       </li>
-      <li class="treeview">
+      <li class="treeview {{ !Route::is('Imagenes.indexE') ?: 'active' }} || {{!Route::is('Imagenes.index') ?: 'active'}}" >
         <a href="#"><i class="fa fa-image"></i> <span>Validar Imágenes</span>
           <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
         </a>
         <ul class="treeview-menu">
-          <li><a href="{{Route('Imagenes.indexE')}}">Empresas</a></li>
-          <li><a href="{{Route('Imagenes.index')}}">Productos</a></li> 
+          <li class="{{ !Route::is('Imagenes.indexE') ?: 'active'}}"><a href="{{Route('Imagenes.indexE')}}">Empresas</a></li>
+          <li class="{{ !Route::is('Imagenes.index') ?: 'active'}}"><a href="{{Route('Imagenes.index')}}">Productos</a></li> 
         </ul>
       </li>
       </ul>
